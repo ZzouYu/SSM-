023b276b24845bb40da8db6481cfe1a8e8cd95b9
zouyu <zouyu@ieforever.com>
2017-09-18 19:10:36 +0800

    1

---
+++ src/cn/wallance/dao/IUserDao.java
@@ -0,0 +1,10 @@
+package cn.wallance.dao;
+
+import java.util.List;
+
+import cn.wallance.domain.User;
+
+public interface IUserDao {
+	//public abstract List<User> selectAll();
+	public abstract List<User> getUser();
+}
